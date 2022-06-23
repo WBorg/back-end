8de4c7f0f0d8c22e6a8afb7b7322c8ffe669469b
Wborg <wilicious@gmail.com>
2022-06-23 18:21:08 -0300

    form-login

---
+++ form-loguin/src/App.jsx
@@ -0,0 +1,18 @@
+
+function App() {
+
+  return (
+    <div>
+      <h1 className="text-center font-bold text-2xl border border-orange-600"
+      >
+        Oi mundo
+      </h1>
+    </div>
+  )
+}
+
+
+
+
+
+export default App
