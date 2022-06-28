ac2bdfab3d6afc2907516931df539b2338e8cf30
Wborg <wilicious@gmail.com>
2022-06-28 17:18:39 -0300

    login-form

---
+++ form-login-back/src/components/Input.jsx
@@ -0,0 +1,18 @@
+import { FormControl, FormLabel, Input as ChakraInput } from "@chakra-ui/react";
+
+
+export function Input({ label, name, ...rest }) {
+  return (
+    <FormControl>
+      <FormLabel htmlFor={name}>{label}</FormLabel>
+      <ChakraInput
+        name={name}
+        focusBorderColor="cyan.500"
+        bgColor="gray.900"
+        variant="outline"
+        {...rest}
+      />
+    </FormControl>
+
+  )
+}
---
+++ form-login-back/src/components/Login.jsx
@@ -0,0 +1,30 @@
+import { Button, Flex, Stack, Link, Spacer } from '@chakra-ui/react';
+import { Input } from '../components/Input';
+
+export  function Login() {
+  return (
+    <Flex w="100vw" h="100vh" align="center" justify="center" bg="blue.900" >
+      <Flex
+        as="form"
+        width="100%"
+        maxW={450}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing="2">
+         <Input name="email" type="email" label="E-mail" id="email" />
+         <Input name="senha" type="password" label="Senha" id="senha" />
+        </Stack>
+        <Flex>
+          <Link color="cyan.800" as='a'>Esqueci minha senha</Link>
+          <Spacer/>
+          <Link color="cyan.800" as='a'>Trocar senha</Link>
+        </Flex>
+
+        <Button type="submit" mt="12" colorScheme="facebook">Entrar</Button>
+      </Flex>
+    </Flex>
+  )
+}
