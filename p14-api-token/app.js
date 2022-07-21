403545bd684d97e4f9c6c3a033d7e592b1a4cdbc
WILLY BORGES SILVA <willy.bsilva@senacsp.edu.br>
2022-07-20 22:35:44 -0300

    api chamada pelo front com erro

--- p14-api-token/app.js
+++ p14-api-token/app.js
@@ -1,21 +1,31 @@
 const express = require('express');
+var cors = require('cors')
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const { promisify } = require('util');
 const app = express();
 const User = require('./models/User');
 require('dotenv').config();
+const { validaToken } = require('./middlewares/auth');
 const sendMail = require('./providers/mailProvider');
+const {userCreateMailTemplate} = require('./template/userCreateMail');
 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}))
 
+app.use((req,res,next)=>{
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers","Content-Type, Authorization");
+    app.use(cors());
+    next();
+})
+
 app.get("/", function (request, response) {
     response.send("Serviço API Rest iniciada...");
 })
 
-app.get("/users", validarToken, async (req, res) =>{
+app.get("/users", validaToken, async (req, res) =>{
     await User.findAll({
         attributes: ['id', 'name', 'email', 'gender', 'password'],
         order:[['name', 'ASC']]
@@ -62,38 +72,22 @@ app.post("/user", async (req, res) => {
 
     var dados = req.body;
     dados.password = await bcrypt.hash(dados.password, 8);
-    let email = dados.email;
-    let name = dados.name;
-    let gender = dados.gender;
 
     await User.create(dados)
     .then( ()=>{
-        /* enviar e-mail */
+        /* enviar e-mail *******************************************/
         let to = email;
         let cc = '';
-        var htmlbody = "";
-        htmlbody += '<div style="background-color:#000; margin-bottom:150px;">';
-        htmlbody += '<div style="margin-top:150px;">';
-        htmlbody += '<p style="color:#fff; font-weight:bold;margin-top:50px;">';
-        htmlbody += 'Olá {name},';
-        htmlbody += '</p>';
-        htmlbody += '<p style="color:#fff; font-style:italic;margin-top:50px;">';
-        htmlbody += 'Sua conta foi criada com sucesso!';
-        htmlbody += '</p>';
-        htmlbody += '<p style="color:#fff;margin-top:50px;">';
-        htmlbody += 'Seu login é o seu email: {email}';
-        htmlbody += '</p>';
-        htmlbody += '<p style="color:#fff;margin-top:50px;">';
-        htmlbody += 'Sexo: {gender}';
-        htmlbody += '</p>';
-        htmlbody += '</div>';
-        htmlbody += '</div>';
-        htmlbody = htmlbody.replace('{name}', name);
-        htmlbody = htmlbody.replace('{email}', email);
-        htmlbody = htmlbody.replace('{gender}', gender);
-        /* ************* */
-        sendMail(to, cc, 'Sua conta foi criada com sucesso!', htmlbody);
-
+        let subject = 'Sua conta foi criada com sucesso!';
+        
+        let mailBody = userCreateMailTemplate({
+            name : dados.name,
+            email: dados.email,
+            gender: dados.gender
+        })
+       
+        sendMail(to, cc, subject , mailBody);
+/************************************************************* */
         return res.json({
             erro: false,
             mensagem: 'Usuário cadastrado com sucesso!'
@@ -168,37 +162,7 @@ app.post("/login", async (req, res) => {
         
     })
 })
- async function validarToken(req, res, next) {
-     const authHeader =  req.headers.authorization;
-     const [bearer , token] = authHeader.split(" ");
-     if(!token){
-        return res.status(400).json({
-            erro: true,
-            menssagem: "Erro: necessário realizar login"
-        })
-     };
-     try{
-        const decoded = await promisify(jwt.verify)(token, process.env.SECRET);
-        req.userId = decoded.id;
-        console.log(req.userId);
-        return next();
-     }catch(err){
-        if(err){
-            
-            return res.status(400).json({
-                erro: true,
-                menssagem: `Erro: ${err}`
-         })
-        }else{
-
-            return res.status(401).json({
-                erro: true,
-                menssagem: "Erro: Necessário realizar login!"
-         })
-        }
-    
-    }
- }
+ 
 
 app.put('/user-senha', async (req, res) => {
     const {id, password } = req.body;
